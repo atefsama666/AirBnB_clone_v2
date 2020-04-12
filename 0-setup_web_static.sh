e4ea18401354f08d35ab153db779938ec27aea03
atefsama666 <mechken.atef@gmail.com>
2020-04-12 01:43:48 +0100

    Shellchecked task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # nginx quick simple setup
-if [[ -z $(dpkg -l | grep "nginx") ]]; then
+v=$(dpkg -l | grep "nginx")
+if [[ -z $v ]]; then
 	apt-get -y update
 	apt-get -y upgrade
 	apt-get -y install nginx
@@ -8,7 +9,7 @@ fi
 mkdir -p /data/web_static/releases/test
 mkdir -p /data/web_static/shared
 simplehtml="<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>"
-echo -e $simplehtml > /data/web_static/releases/test/index.html
+echo -e "$simplehtml" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 location="\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}"
