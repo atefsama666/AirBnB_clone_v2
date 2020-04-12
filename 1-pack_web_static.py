51cab628c127c708dd0aab71256aba4fe4072d27
atefsama666 <mechken.atef@gmail.com>
2020-04-12 02:40:15 +0100

    Task 1 pep8 checked

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,18 +2,19 @@
 '''Module to compress folder into tgz'''
 import fabric
 import datetime
+
+
 def do_pack():
-	'''Packs a folder into tgz'''
-	try:
-		fabric.operations.local('mkdir -p versions')
-		name = str(datetime.datetime.now())
-		name = name.replace("-", "")
-		name = name.replace(" ", "")
-		name = name[:16]
-		name = name.replace(":", "")
-		name = "versions/web_static_" + name
-		fabric.operations.local("tar -czvf {}.tgz web_static".format(name))
-		return name
-	except:
-		return None
-	
+    '''Packs a folder into tgz'''
+    try:
+        fabric.operations.local('mkdir -p versions')
+        name = str(datetime.datetime.now())
+        name = name.replace("-", "")
+        name = name.replace(" ", "")
+        name = name[:16]
+        name = name.replace(":", "")
+        name = "versions/web_static_" + name
+        fabric.operations.local("tar -czvf {}.tgz web_static".format(name))
+        return name
+    except:
+        return None
