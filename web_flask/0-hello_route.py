277825c602685fbfe76d5de4163e954a6487843a
atefsama666 <mechken.atef@gmail.com>
2020-06-15 12:19:38 +0100

    Task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""Python Module"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def helloHbnb():
+	"""Python Method"""
     return 'Hello HBNB!'
 
 
