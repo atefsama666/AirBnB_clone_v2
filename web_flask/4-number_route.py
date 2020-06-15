b64c7df00e960121df299820f40c835d136f8ddc
atefsama666 <mechken.atef@gmail.com>
2020-06-15 14:45:11 +0100

    Task 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Python Module"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def helloHbnb():
+    """Python Method"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Python Method"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cfunc(text):
+    """Python Method"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """Python Method"""
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:number>', strict_slashes=False)
+def number(number):
+    """Python Method"""
+    return '{} is a number'.format(number)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
